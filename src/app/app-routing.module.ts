7ded2af2aa83ef63257e15ab829c1c61620da654
HectorCo <hcompanyortega@gmail.com>
2019-07-09 12:12:37 +0200

    Iteracion 2 OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyhomeComponent } from '../app/myhome/myhome.component';
+import { MymovieComponent } from '../app/mymovie/mymovie.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: 'myhome', pathMatch: 'full'},
+  {path: 'myhome', component: MyhomeComponent},
+  {path: 'movie/:id', component: MymovieComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyhomeComponent } from './myhome/myhome.component';
+import { MymovieComponent } from './mymovie/mymovie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyhomeComponent,
+    MymovieComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [MyhomeComponent, MymovieComponent]
 })
 export class AppModule { }
