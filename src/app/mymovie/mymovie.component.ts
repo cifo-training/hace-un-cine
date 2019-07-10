862a154059fa10ad78713fe450c763ecd3ba5929
HectorCo <hcompanyortega@gmail.com>
2019-07-10 13:49:06 +0200

    Iteracion-4 OK The End

--- src/app/mymovie/mymovie.component.ts
+++ src/app/mymovie/mymovie.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from '../services/movies.service';
 
 @Component({
   selector: 'app-mymovie',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mymovie.component.scss']
 })
 export class MymovieComponent implements OnInit {
+  public id: any;
+  public movie: any = {};
 
-  constructor() { }
+  constructor(
+    public movieService: MoviesService,
+    public route: ActivatedRoute
+    ) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.id = params.id;
+      this.movie = this.movieService.getMovie(this.id);
+    });
   }
 
 }
