b8a5c40ce33f62b7fd6ec2c4f3971fb8d94a36dd
HectorCo <hcompanyortega@gmail.com>
2019-07-10 13:54:02 +0200

    arreglos

--- src/app/movies-list.ts
+++ src/app/movies-list.ts
@@ -15,6 +15,7 @@ const movies: Array<any> = [
     id: 1,
       title: 'The Shawshank Redemption',
       poster: 'https://i.pinimg.com/originals/da/0d/cc/da0dcc87e0e8b776a5f88ef460834db1.jpg',
+// tslint:disable-next-line: max-line-length
       synopsis: 'In 1947, Andy Dufresne (Tim Robbins), a banker in Maine, is convicted of murdering his wife and her lover, a golf pro. Since the state of Maine has no death...',
       genres: [
         'Crime', 'Drama'
@@ -38,7 +39,9 @@ const movies: Array<any> = [
     {
       id: 2,
       title: 'The Godfather',
+// tslint:disable-next-line: max-line-length
       poster: 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
+// tslint:disable-next-line: max-line-length
       synopsis: 'In late summer 1945, guests are gathered for the wedding reception of Don Vito Corleone\'s daughter Connie (Talia Shire) and Carlo Rizzi (Gianni Russo). Vito...',
       genres: [
         'Crime', 'Drama'
@@ -61,8 +64,10 @@ const movies: Array<any> = [
     {
       id: 3,
       title: 'The Godfather Part II',
+// tslint:disable-next-line: max-line-length
       poster: 'https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
-      synopsis: 'The Godfather Part II presents two parallel storylines. One involves Mafia chief Michael Corleone in 1958/1959 after the events of the first movie; the othe...',
+// tslint:disable-next-line: max-line-length
+      synopsis: 'The Godfather Part II presents two parallel storylines. One involves Mafia chief Michael Corleone in 1958/1959 after the events of the first movie; the other...',
       genres: [
         'Crime', 'Drama'
       ],
@@ -86,6 +91,7 @@ const movies: Array<any> = [
       id: 4,
       title: 'The Dark Knight',
       poster: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg',
+// tslint:disable-next-line: max-line-length
       synopsis: 'The movie begins with a gang of men with clown masks breaking into the bank where the mob has a large portion of their money stashed. It begins with five cl...',
       genres: [
         'Action', 'Crime', 'Drama', 'Thriller'
@@ -109,7 +115,9 @@ const movies: Array<any> = [
     {
       id: 5,
       title: 'Schindler\'s List',
+// tslint:disable-next-line: max-line-length
       poster: 'https://m.media-amazon.com/images/M/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg',
+// tslint:disable-next-line: max-line-length
       synopsis: 'The relocation of Polish Jews from surrounding areas to Krakow begins in late 1939, shortly after the outbreak of World War II, when the German Army defeats...',
       genres: [
         'Biography', 'Drama', 'History'
